1a9b357a0700ad675a5ecc5833236d297ae35c7c
Tejas Sabunkar <tsabunkar@gmail.com>
2018-12-25 21:04:47 +0530

    updating config file

--- server/config/config.js
+++ server/config/config.js
@@ -3,13 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 if (env === 'development' || env === 'test') {
 
 
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb+srv://tejas-admin:icui4cuNOW@mycluster-0mtaj.mongodb.net/postMessagesdb?retryWrites=true';
-    process.env.JWT_SECRET = 'mySalt123!';
-    process.env.ENV = 'development';
-
-
-   /*  const config = require('./config.json');
+    const config = require('./config.json');
 
     if (env === 'development') { // development env
         process.env.PORT = config.development.PORT;
@@ -24,6 +18,6 @@ if (env === 'development' || env === 'test') {
         process.env.MONGODB_URI = config.test.MONGODB_URI;
         process.env.JWT_SECRET = config.test.JWT_SECRET;
         process.env.ENV = config.test.ENV;
-    } */
+    }
 
 }
